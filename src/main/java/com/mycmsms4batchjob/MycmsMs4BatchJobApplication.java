e7115f440ebb24520081352a21a74002f0d8c6c8
DzakirinMD <dzakirinmd@gmail.com>
2023-06-01 08:21:59 +0800

    Added base project

---
+++ src/main/java/com/mycmsms4batchjob/MycmsMs4BatchJobApplication.java
@@ -0,0 +1,13 @@
+package com.mycmsms4batchjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycmsMs4BatchJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycmsMs4BatchJobApplication.class, args);
+	}
+
+}
